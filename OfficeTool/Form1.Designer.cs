31207c31d6bb5a8e8c86bdd1ca72ff792b94f019
melon_pro <389201492@qq.com_pro>
2019-05-02 23:02:54 +0800

    完成基础框架

---
+++ OfficeTool/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TestExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.btnReadExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Location = new System.Drawing.Point(7, 7);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.RowTemplate.Height = 27;
+            this.dgvShow.Size = new System.Drawing.Size(512, 424);
+            this.dgvShow.TabIndex = 0;
+            // 
+            // btnReadExcel
+            // 
+            this.btnReadExcel.Location = new System.Drawing.Point(596, 28);
+            this.btnReadExcel.Name = "btnReadExcel";
+            this.btnReadExcel.Size = new System.Drawing.Size(100, 36);
+            this.btnReadExcel.TabIndex = 1;
+            this.btnReadExcel.Text = "读取excel";
+            this.btnReadExcel.UseVisualStyleBackColor = true;
+            this.btnReadExcel.Click += new System.EventHandler(this.btnReadExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 440);
+            this.Controls.Add(this.btnReadExcel);
+            this.Controls.Add(this.dgvShow);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.Button btnReadExcel;
+    }
+}
+
---
+++ OfficeTool/Form1.cs
@@ -0,0 +1,26 @@
+﻿using OfficeTool.Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReadExcel_Click(object sender, EventArgs e)
+        {
+            dgvShow.DataSource= ExcelHelper.ExcelToDataTable("C:/Users/melon/Desktop/123.xls", true);
+        }
+    }
+}
